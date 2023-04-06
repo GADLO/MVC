31acad6c8b98fbb208580710b78bb60e17c2ec12
GADLO <geekarea@outlook.com>
2023-04-06 11:01:58 +0800

    ejs

--- server/app.js
+++ server/app.js
@@ -1,13 +1,89 @@
 const express = require("express");
 const { join } = require("path");
+const ejs = require("ejs");
 
 const port = 8000;
 const app = express();
+
+app.set("view engine", "ejs");
 
 //配置服务器映射到静态文件夹Public
 app.use(express.static(join(__dirname, "public")));
 
 //服务监听
 app.listen(port, () => {
-  console.log(`server is running at port ${port}`);
+  console.log(`server is listening at port ${port}`);
+});
+
+//利用路由回调动态渲染
+app.get("/", (req, res) => {
+  const title = "Server Side Render";
+  const lists = [
+    {
+      id: 0,
+      name: "Server",
+    },
+    {
+      id: 1,
+      name: "Side",
+    },
+    {
+      id: 2,
+      name: "Render",
+    },
+  ];
+  res.end(
+    `<!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+            <link rel="stylesheet" href="./css/static.css">
+          </head>
+        <body>
+          <div class="title">
+           ${title}
+          </div>
+          <div class="list">
+            <ul id="J_list">
+            ${lists
+              .map(
+                (item) => `<li>
+                    <span>${item.id}</span>
+                    <p>${item.name}</p>
+                    </li>`
+              )
+              .join("")}
+            </ul>
+          </div>
+        </body>
+     
+        </html>`
+  );
+});
+
+//利用模版引擎动态渲染
+app.get("/ejs", (req, res) => {
+  const title = "EJS Server Side Render";
+  const lists = [
+    {
+      id: 0,
+      name: "EJS Server",
+    },
+    {
+      id: 1,
+      name: "Side",
+    },
+    {
+      id: 2,
+      name: "Render",
+    },
+  ];
+
+  res.render("index", {
+    title,
+    lists,
+  });
 });
